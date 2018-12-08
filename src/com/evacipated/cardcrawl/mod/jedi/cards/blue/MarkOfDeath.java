021fde3e281b22bc72560ca123b8fdfefa724015
Jedi515 <44167564+Jedi515@users.noreply.github.com>
2018-12-08 21:42:37 +0900

    Add files via upload

---
+++ src/com/evacipated/cardcrawl/mod/jedi/cards/blue/MarkOfDeath.java
@@ -0,0 +1,42 @@
+package com.evacipated.cardcrawl.mod.jedi.cards.blue;
+
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.LockOnPower;
+
+public class MarkOfDeath extends CustomCard
+{
+    public static final String ID = "jedi:markofdeath";
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+    public MarkOfDeath()
+    {
+        super(ID, NAME, "resources/images/cards/jedi_beta.png", 1, DESCRIPTION, CardType.SKILL, CardColor.BLUE, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        this.exhaust = true;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(m, p, new LockOnPower(m, 99), 99));
+    }
+
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeBaseCost(0);
+        }
+
+    }
+
+    public AbstractCard makeCopy()
+    {
+        return new MarkOfDeath();
+    }
+}
--- src/com/evacipated/cardcrawl/mod/jedi/cards/blue/crystallizedlightning.java
+++ src/com/evacipated/cardcrawl/mod/jedi/cards/blue/crystallizedlightning.java
@@ -3,15 +3,21 @@ package com.evacipated.cardcrawl.mod.jedi.cards.blue;
 import basemod.abstracts.CustomCard;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.ElectroPower;
 import com.megacrit.cardcrawl.powers.FocusPower;
+import com.megacrit.cardcrawl.powers.LockOnPower;
 import com.megacrit.cardcrawl.powers.StrengthPower;
+
+import java.util.ArrayList;
 
 
 public class crystallizedlightning
@@ -27,20 +33,38 @@ public class crystallizedlightning
     {
         super(ID, NAME, "resources/images/cards/jedi_beta_attack.png", 1, DESCRIPTION, CardType.ATTACK, CardColor.BLUE, CardRarity.COMMON, CardTarget.ENEMY);
         this.baseDamage = 7;
+        this.baseMagicNumber = 1;
+        this.magicNumber = this.baseMagicNumber;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m)
     {
-        AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
+        if (p.hasPower(ElectroPower.POWER_ID))
+        {
+            AbstractDungeon.actionManager.addToBottom(new DamageAllEnemiesAction(m, this.multiDamage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
+        }
+        else
+        {
+            AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
+        }
     }
 
     @Override
     public void applyPowers() {
+        if (AbstractDungeon.player.hasPower(ElectroPower.POWER_ID)) {
+            this.target = AbstractCard.CardTarget.ALL_ENEMY;
+            this.isMultiDamage = true;
+        }
+        else {
+            this.target = AbstractCard.CardTarget.ENEMY;
+            this.isMultiDamage = false;
+        }
         int baseDamagePlaceholder = this.baseDamage;
         int currentFocus = 0;
         int currentStrength = 0;
         boolean hasFocus = false;
         boolean hasStrength = false;
+
         if (AbstractDungeon.player.hasPower(FocusPower.POWER_ID)) {
             currentFocus = AbstractDungeon.player.getPower(FocusPower.POWER_ID).amount;
             hasFocus = true;
@@ -50,18 +74,18 @@ public class crystallizedlightning
             hasStrength = true;
         }
         if (hasFocus) {
-            if (this.upgraded || !hasStrength) {
+            if (!hasStrength) {
                 this.baseDamage += currentFocus;
                 super.applyPowers();
                 this.baseDamage = baseDamagePlaceholder;
                 this.isDamageModified = baseDamage != damage;
             } else {
-                AbstractDungeon.player.getPower(StrengthPower.POWER_ID).amount = currentFocus;
+                AbstractDungeon.player.getPower(StrengthPower.POWER_ID).amount = currentFocus * magicNumber;
                 super.applyPowers();
                 AbstractDungeon.player.getPower(StrengthPower.POWER_ID).amount = currentStrength;
             }
-        } else if (hasStrength && !this.upgraded) {
-            AbstractDungeon.player.getPower(StrengthPower.POWER_ID).amount = currentFocus;
+        } else if (hasStrength) {
+            AbstractDungeon.player.getPower(StrengthPower.POWER_ID).amount = currentFocus * magicNumber;
             super.applyPowers();
             AbstractDungeon.player.getPower(StrengthPower.POWER_ID).amount = currentStrength;
         } else {
@@ -71,11 +95,24 @@ public class crystallizedlightning
 
     @Override
     public void calculateCardDamage(AbstractMonster m) {
+        if (AbstractDungeon.player.hasPower(ElectroPower.POWER_ID)) {
+            this.target = AbstractCard.CardTarget.ALL_ENEMY;
+            this.isMultiDamage = true;
+        }
+        else {
+            this.target = AbstractCard.CardTarget.ENEMY;
+            this.isMultiDamage = false;
+        }
         int baseDamagePlaceholder = this.baseDamage;
         int currentFocus = 0;
         int currentStrength = 0;
         boolean hasFocus = false;
         boolean hasStrength = false;
+//        boolean hasLockon = false;
+//        ArrayList<AbstractMonster> monArr = AbstractDungeon.getCurrRoom().monsters.monsters;
+//        why? because when it turns into multi-hit, it nulls m from this function and gets NPE if it's multihit and there's anyone with lockon
+//        int monArrSize = monArr.size();
+
         if (AbstractDungeon.player.hasPower(FocusPower.POWER_ID)) {
             currentFocus = AbstractDungeon.player.getPower(FocusPower.POWER_ID).amount;
             hasFocus = true;
@@ -84,19 +121,20 @@ public class crystallizedlightning
             currentStrength = AbstractDungeon.player.getPower(StrengthPower.POWER_ID).amount;
             hasStrength = true;
         }
+
         if (hasFocus) {
-            if (this.upgraded || !hasStrength) {
+            if (!hasStrength) {
                 this.baseDamage += currentFocus;
                 super.calculateCardDamage(m);
                 this.baseDamage = baseDamagePlaceholder;
                 this.isDamageModified = baseDamage != damage;
             } else {
-                AbstractDungeon.player.getPower(StrengthPower.POWER_ID).amount = currentFocus;
+                AbstractDungeon.player.getPower(StrengthPower.POWER_ID).amount = currentFocus * magicNumber;
                 super.calculateCardDamage(m);
                 AbstractDungeon.player.getPower(StrengthPower.POWER_ID).amount = currentStrength;
             }
-        } else if (hasStrength && !this.upgraded) {
-            AbstractDungeon.player.getPower(StrengthPower.POWER_ID).amount = currentFocus;
+        } else if (hasStrength) {
+            AbstractDungeon.player.getPower(StrengthPower.POWER_ID).amount = currentFocus * magicNumber;
             super.calculateCardDamage(m);
             AbstractDungeon.player.getPower(StrengthPower.POWER_ID).amount = currentStrength;
         } else {
@@ -109,6 +147,7 @@ public class crystallizedlightning
         if(!this.upgraded)
         {
             upgradeName();
+            this.upgradeMagicNumber(1);
             this.rawDescription = UPGRADE_DESCRIPTION;
             initializeDescription();
         }
