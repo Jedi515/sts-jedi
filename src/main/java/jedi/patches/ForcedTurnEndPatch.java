5df955df2dd880f822f814f4163f7344cdcec89b
Jedi515 <44167564+Jedi515@users.noreply.github.com>
2023-09-14 09:13:40 +1000

    Update ForcedTurnEndPatch.java
    IS:Nightmares are no longer immune to denying turn end.

--- src/main/java/jedi/patches/ForcedTurnEndPatch.java
+++ src/main/java/jedi/patches/ForcedTurnEndPatch.java
@@ -1,21 +1,52 @@
 package jedi.patches;
 
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2;
-import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
+import com.evacipated.cardcrawl.modthespire.lib.*;
+import com.evacipated.cardcrawl.modthespire.patcher.PatchingException;
 import com.megacrit.cardcrawl.actions.GameActionManager;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.screens.select.GridCardSelectScreen;
+import javassist.CannotCompileException;
+import javassist.CtBehavior;
+import javassist.expr.ExprEditor;
+import javassist.expr.MethodCall;
 import jedi.interfaces.onForcedTurnEndInterface;
 
-@SpirePatch2(clz = GameActionManager.class, method = "callEndTurnEarlySequence")
+import java.util.ArrayList;
+
 public class ForcedTurnEndPatch {
-    public static SpireReturn Prefix()
-    {
-        for (AbstractRelic r : AbstractDungeon.player.relics)
-        {
-            if ((r instanceof onForcedTurnEndInterface) && (!(((onForcedTurnEndInterface) r).onForcedTurnEnd())))
-                return SpireReturn.Return();
+
+    @SpirePatch2(clz = GameActionManager.class, method = "callEndTurnEarlySequence")
+    public static class BaseGame {
+        public static SpireReturn Prefix() {
+            for (AbstractRelic r : AbstractDungeon.player.relics) {
+                if ((r instanceof onForcedTurnEndInterface) && (!(((onForcedTurnEndInterface) r).onForcedTurnEnd())))
+                    return SpireReturn.Return();
+            }
+            return SpireReturn.Continue();
         }
-        return SpireReturn.Continue();
+    }
+
+    @SpirePatch2(cls = "infinitespire.monsters.Nightmare", method = "onEnoughDamageTakenAlpha", optional = true)
+    @SpirePatch2(cls = "infinitespire.monsters.Nightmare", method = "onEnoughDamageTakenBeta", optional = true)
+    public static class InfiniteSpire {
+        @SpireInsertPatch(locator = Locator.class)
+        public static SpireReturn Insert() {
+            for (AbstractRelic r : AbstractDungeon.player.relics) {
+                if ((r instanceof onForcedTurnEndInterface) && (!(((onForcedTurnEndInterface) r).onForcedTurnEnd())))
+                    return SpireReturn.Return();
+            }
+            return SpireReturn.Continue();
+        }
+    }
+
+
+    private static class Locator extends SpireInsertLocator
+    {
+        public int[] Locate(CtBehavior ctMethodToPatch) throws CannotCompileException, PatchingException
+        {
+            Matcher finalMatcher = new Matcher.MethodCallMatcher(ArrayList.class, "clear");
+            return LineFinder.findInOrder(ctMethodToPatch, finalMatcher);
+        }
     }
 }
